1443a44691d3040c50a40e5fb1d0a468612d1ff8
Juan Pablo Mayoral <jpmayoral@gmail.com>
2017-03-10 15:57:17 -0300

    Agrego fix envio de mail despues de aplicar Costo Financiero

--- app/code/community/Todopago/Modulodepago2/controllers/PaymentController.php
+++ app/code/community/Todopago/Modulodepago2/controllers/PaymentController.php
@@ -346,14 +346,6 @@ public function lastStep($order_key, $answer_key){
             }
             $order->setState($state, $status, $message);
 
-            try{
-                $order->sendNewOrderEmail();
-            }catch(Exception $e){
-                $todopagoLog->log("catch : ".__METHOD__);
-                $todopagoLog->log("message: ".var_export($e, true));
-                $order->sendOrderUpdateEmail(true, $message);
-            }
-
             /*costo financiero*/
             $amountBuyer = isset($second_step['Payload']['Request']['AMOUNTBUYER'])?$second_step['Payload']['Request']['AMOUNTBUYER']:number_format($order->getGrandTotal(), 2, ".", "");
             $cf = $amountBuyer - $order->getGrandTotal();
@@ -363,6 +355,14 @@ public function lastStep($order_key, $answer_key){
             $order->setBaseGrandTotal($amountBuyer);
 
             $order->save();
+
+            try{
+                $order->sendNewOrderEmail();
+            }catch(Exception $e){
+                $todopagoLog->log("catch : ".__METHOD__);
+                $todopagoLog->log("message: ".var_export($e, true));
+                $order->sendOrderUpdateEmail(true, $message);
+            }
 
             $payment = $order->getPayment();
             $payment->setTransactionId($second_step['AuthorizationKey']);
